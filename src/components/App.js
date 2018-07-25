4a4949857abdaffe2054fa5384f21c390ba4ec4f
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-07-25 16:37:40 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Navbar,Nav,NavItem,MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './App.css';
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.toggleAddTodo = this.toggleAddTodo.bind(this);
+  }
+toggleAddTodo(e){
+    e.preventDefault();
+     this.props.mappedToggleAddTodo();
+  }
+render(){
+    return(
+      <div>
+      <Navbar inverse  collapseOnSelect className="customNav">
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/#">Mern Stack Todo App</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <LinkContainer to={{ pathname: '/', query: {  } }}>
+           <NavItem eventKey={1}>Home</NavItem>
+        </LinkContainer>
+      </Nav>
+      <Nav pullRight>
+      <LinkContainer to={{ pathname: '/', query: {  } }} onClick={this.toggleAddTodo}>
+         <NavItem eventKey={1}>Add Todo</NavItem>
+      </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  <div className="container">
+  { /* Each Smaller Components */}
+   {this.props.children}
+  </div>
+ </div>
+    );
+  }
+}
